3ad8a222e4482c722abe883adb204a824fb4d88a
KVadim-K <kvadimk@yandex.ru>
2024-05-07 23:52:47 +0300

    new code

--- new.py
+++ new.py
@@ -1,14 +1,1 @@
-
-print("Я конфликтую!")
-for i in range(10):
-    print(слово)
-
-a,b = 9, 8
-b = 5
-if a > b:
-    print("a больше b")
-elif a < b:
-    print("a меньше b")
-else:
-    print("a равно b")
-
+ a = 5
